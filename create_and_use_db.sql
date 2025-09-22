99c58538cbb893b8c5ebd74d7a6bd51593b860fe
gutierrezjovaniebsit-creator <gutierrejovanie_bsit@plmun.edu.ph>
2025-09-22 22:31:35 +0800

    first commit

---
+++ create_and_use_db.sql
@@ -0,0 +1,8 @@
+-- Step 1: Create a database called UniversityDB
+CREATE DATABASE `UniversityDB`;
+
+-- Step 2: Use the newly created database
+USE `UniversityDB`;
+
+-- Optional Verification: Confirm the selected database
+SELECT DATABASE();
---
+++ drop_db.sql
@@ -0,0 +1,8 @@
+-- Step 3: Drop the database if needed.
+-- ⚠️ WARNING: This operation is destructive and cannot be undone!
+
+-- Ensure you're in the correct context
+USE `UniversityDB`;
+
+-- Drop the database only if it exists
+DROP DATABASE IF EXISTS `UniversityDB`;
